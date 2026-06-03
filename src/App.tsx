a093ae5c10b5bb7d0d5604ceb81e0fe716163ea9
Jiwoon Jung <wldns9368@naver.com>
2026-06-03 16:45:25 +0900

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { TodoProvider } from './context/TodoContext';
 import { Navbar } from './components/Navbar';
